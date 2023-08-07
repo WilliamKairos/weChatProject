6c5bee8286df128abac9d532113def2e1504233c
WiliamKairos <wwilliamkairos@gmail.com>
2023-08-07 21:23:06 +0800

    跨域

---
+++ src/main/java/com/wechat/WeChatProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeChatProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeChatProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wechat/config/CorsConfig.java
@@ -0,0 +1,4 @@
+package com.wechat.config;
+
+public class CorsConfig {
+}
